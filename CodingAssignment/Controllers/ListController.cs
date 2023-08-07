9eb9584cbdf7e92c2657c6ce9b5d8bfc9152105e
lokeshkumars22 <97249827+lokeshkumars22@users.noreply.github.com>
2023-08-08 01:04:18 +0530

    Add files via upload

---
+++ CodingAssignment/Controllers/ListController.cs
@@ -0,0 +1,93 @@
+﻿using CodingTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+
+namespace CodingTest.Controllers
+{
+    [ApiController]
+    public class ListController : ControllerBase
+    {
+        private static List<ListObjects> lstObjects = new List<ListObjects>
+            {
+                new ListObjects
+                {
+                    Id=1,
+                    Name="Lokesh"
+                },
+                new ListObjects
+                {
+                    Id=2,
+                    Name="Kumar"
+                }
+            };
+        
+        /// <summary>
+        /// This method inserts new object to the list
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [Route("/api/list")]
+        [HttpPost]
+        public IActionResult PostObjects([FromBody] ListObjects obj)
+        {
+            lstObjects.Add(obj);
+            return Ok(lstObjects);
+        }
+
+        /// <summary>
+        /// This method fetches specific object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("/api/list/{id}")]
+        [HttpGet]
+        public IActionResult GetObjectsById(int id)
+        {
+            var obj = lstObjects.Find(x => x.Id == id);
+            if (obj is null)
+                return NotFound();
+
+            return Ok(obj);
+        }
+
+        /// <summary>
+        /// This method fetches all the objects
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/list")]
+        [HttpGet]
+        public IActionResult GetAllObjects()
+        {
+          return Ok(lstObjects);
+        }
+
+
+        [Route("/api/list/{id}? name = {new name}")]
+        [HttpPatch]
+        public IActionResult PatchObjects([FromBody] int id, JsonPatchDocument jsonPatchDocument)
+        {
+            var obj = lstObjects.Find(x => x.Id == id);
+            return Ok(obj);
+        }
+
+        /// <summary>
+        /// This method deletes the object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("/api/list/{id}")]
+        [HttpDelete]
+        public IActionResult DeleteObject(int id)
+        {
+            var obj = lstObjects.Find(x => x.Id == id);
+            if (obj is null)
+                return NotFound();
+
+            lstObjects.Remove(obj);
+            return Ok(obj);
+        }
+    }
+}
---
+++ CodingAssignment/Models/ListObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingTest.Models
+{
+    public class ListObjects
+    {
+        public int Id { get; set; }
+        [StringLength(10)]
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
